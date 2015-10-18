43d748334ef32675e85cf8e3204a3bd25bcc527b
David Lazăr <dlazar@gmail.com>
2015-10-18 19:11:23 +0200

    Adds setup.py.  Reorganizes the source for stdeb.
    Build debian package with:
    
        $ ./setup.py --command-packages=stdeb.command bdist_deb

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+from DistUtilsExtra.auto import setup
+
+setup(
+    name = 'same-ball',
+    version = '0.9.3',
+    author = 'David Lazăr',
+    author_email = 'dlazar@gmail.com',
+    license = 'Apache License 2.0',
+    description = 'Colored balls puzzle game',
+    long_description = 'Remove groups of two or more balls of the same '
+                       'color and try to clear the board.',
+    url = 'http://undef.ro/other/same-ball/',
+    scripts = ['same-ball'],
+    provides = [],
+)
